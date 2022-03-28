3d4ccbb8d8adf511d4733c753c16fea4107967d6
kispols <kispal.attila0106@gmail.com>
2022-03-28 10:31:24 +0200

    3.feladat

--- 2022-03-28_Balkezesek/Program.cs
+++ 2022-03-28_Balkezesek/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _2022_03_28_Balkezesek
 {
@@ -10,6 +11,15 @@ namespace _2022_03_28_Balkezesek
     {
         static void Main(string[] args)
         {
+            List<Sportoló> sportolók = new List<Sportoló>();
+
+            foreach (var sor in File.ReadAllLines("balkezesek.csv").Skip(1))
+            {
+                sportolók.Add(new Sportoló(sor));
+            }
+
+            Console.WriteLine($"3. feladat: {sportolók.Count}");
+            Console.ReadKey();
         }
     }
 }
---
+++ 2022-03-28_Balkezesek/Sportoló.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_03_28_Balkezesek
+{
+    class Sportoló
+    {
+        public string név { get; set; }
+        public string első { get; set; }
+        public string utolsó { get; set; }
+        public int súly { get; set; }
+        public int magasság { get; set; }
+
+
+        public Sportoló(string sor )
+        {
+            string[] t = sor.Split(';');
+            név = t[0];
+            első = t[1];
+            utolsó = t[2];
+            súly = int.Parse(t[3]);
+            magasság = int.Parse(t[4]);
+        }
+    }
+}
